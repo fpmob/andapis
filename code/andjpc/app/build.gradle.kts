123783176c6433640f62fdd2a4fb38d5858f5e4a
Christopher Augustus <repos@c4augustus.com>
2025-02-12 18:37:27 +0100

    upgrade build compileSdk and targetSdk to 36

--- code/andjpc/app/build.gradle.kts
+++ code/andjpc/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "org.andapis"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.andapis"
         minSdk = 35
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
